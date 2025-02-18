f892cf2f958ca4ea63a0aba31d85e6a1d1c8a3e9
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-18 23:31:43 +0530

    main.py: image to text api implemented

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+
+import pytesseract
+from PIL import Image
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Cannot GET, send image file in the post method"}
+
+@app.post("/")
+async def image_url(file: UploadFile = File(...)):
+    if file:
+        img = Image.open(file)
+        text = pytesseract.image_to_string(img)
+        return {"content": text}
+    else:
+        return {"message": "please send image_url"}
