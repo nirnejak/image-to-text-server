cc66d20715e2e181b917be10f3fa55e6bbaf631f
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-19 00:01:51 +0530

    user router added

---
+++ routers/users.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+users = [{ "username": "Rick" }, { "username": "Morty" }]
+
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return users
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return { "username": "currentuser" }
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return { "username": username }
