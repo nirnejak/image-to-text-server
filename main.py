038fe1bb94fc34f66fdd4fd658b12828407c5d48
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-02-18 23:32:23 +0530

    main.py: formatting

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Cannot GET, send image file in the post method"}
+    return { "message": "Cannot GET, send image file in the post method" }
 
 @app.post("/")
 async def image_url(file: UploadFile = File(...)):
     if file:
         img = Image.open(file)
         text = pytesseract.image_to_string(img)
-        return {"content": text}
+        return { "content": text }
     else:
-        return {"message": "please send image_url"}
+        return { "message": "please send image_url" }
